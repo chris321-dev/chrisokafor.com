c1b07bdc462d57bce1b24c53c0a65b254a1f2252
Christian Okafor <okaforchristian08@gmail.com>
2024-03-22 05:50:35 +0100

    Update script.js
    added nav scroll active link

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ window.addEventListener("load", () => {
     }, 200)
 });
 
+
 // Get the button
 var mybutton = document.getElementById("backToTopBtn");
 
@@ -28,3 +29,28 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+$(document).ready(function(){
+  // Add smooth scrolling on all links inside the navbar
+  $(".navbar a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  // Add scrollspy to navbar
+  $('body').scrollspy({target: ".navbar", offset: 100});
+
+  // Add active class to navbar links on scroll
+  $(".navbar-nav a").on('click', function() {
+    $(".navbar-nav").find(".active").removeClass("active");
+    $(this).addClass("active");
+  });
+});
